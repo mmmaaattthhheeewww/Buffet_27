39377aab53f01672c9c3ea4b9491817fcbfea830
mmmaaattthhheeewww <matthewamirkhanian7@gmail.com>
2026-09-08 18:02:22 +0000

    update

--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -10,5 +10,7 @@ class starter {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
 		System.out.print("I love to learn coding remotely."); 
+		String bob=("happerdappergappermapperzappersapper");
+		System.out.println(bob);
 	}
 }
