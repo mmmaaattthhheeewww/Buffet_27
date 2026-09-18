b50ae373ced0d36a11a7493a9290d5af96314f52
mmmaaattthhheeewww <matthewamirkhanian7@gmail.com>
2026-09-18 17:56:03 +0000

    update

--- Java/000_Labs/009_math_methods/base_code/starter.java
+++ Java/000_Labs/009_math_methods/base_code/starter.java
@@ -7,8 +7,19 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int x = (int)Math.max(13 - 6 * 11, 30 % 7 * (-2));
+		System.out.println("Maximum = " + x);
+		double y =(double)Math.sqrt(3 * 8 + 31 % 7);
+		System.out.println("Sqrt= " + y);
+		double z =(double)Math.pow(37 / 3, 35 % 21);
+		System.out.println("Pow = " + z);
+		double u =(double)Math.max(Math.pow( 2,14%3)Math.sqrt ( 2*6));
+		System.out.println("max = " + u);
+
+
+		
+		
+		 
+
 	}
 }
--- Java/102_Exercises/09_Next5/base_code/starter.java
+++ Java/102_Exercises/09_Next5/base_code/starter.java
@@ -7,7 +7,32 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-
-
+		System.out.print("Please enter a number:");
+		Scanner sc = new Scanner(System.in);
+		int numberafter = sc.nextInt();
+		System.out.println("Here are the next 5 numbers:");
+		System.out.print(numberafter + 1);
+		System.out.print(",");
+		System.out.print(numberafter + 2);
+		System.out.print(",");
+		System.out.print(numberafter + 3);
+		System.out.print(",");
+		System.out.print(numberafter + 4);
+		System.out.print(",");
+		System.out.println(numberafter + 5);
+		System.out.println("Here are the next 5 multiples: ");
+		System.out.print(numberafter*1);
+		System.out.print(",");
+		System.out.print(numberafter*2);
+		System.out.print(",");
+		System.out.print(numberafter*3);
+		System.out.print(",");
+		System.out.print(numberafter*4);
+		System.out.print(",");
+		System.out.println(numberafter*5);
+		System.out.println("Here is " + numberafter + " divided by 100");
+		System.out.println(numberafter / 100.0);
+		System.out.println ("Here is " + numberafter + " divided by 10");
+		System.out.println(numberafter / 10.0);
 	}
 }
